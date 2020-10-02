13ba846126435109d6e6532971b246c12532d94f
Anna Butkova <44586010+annabutkova@users.noreply.github.com>
2020-10-03 02:46:03 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
 exports.min = function min (array) {
-  return 0;
+  if ( typeof array == 'undefined' || array.length === 0 ) {
+    return 0;
+  } else {
+    array.sort((a,b) => a-b);
+  return array[0];
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( typeof array == 'undefined' || array.length === 0 ) {
+    return 0;
+  } else {
+    array.sort((a,b) => a-b);
+  return array[array.length-1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  let sum = 0;
+  if ( typeof array == 'undefined' || array.length === 0 ) {
+    return 0;
+  } else {
+    
+    array.forEach(function(elem) {
+      sum += elem;
+    });
+    return sum/array.length;    
+  }
 }
